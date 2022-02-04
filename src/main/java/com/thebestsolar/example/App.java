78dd3cf28f9f6a99a6ac596790d36a3eb7a8d560
Hubert Pei <hubert.pei@sap.com>
2022-02-04 11:44:35 -0800

    new java project

---
+++ src/main/java/com/thebestsolar/example/App.java
@@ -0,0 +1,13 @@
+package com.thebestsolar.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWrold 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
